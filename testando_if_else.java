26d2b9d3691695f2f99ce5f7046d858b3ec787d1
Aliane Elias Amaral <108065159+AlianeElias@users.noreply.github.com>
2022-07-21 08:25:45 -0400

    Add files via upload
    Primeiro código utilizando if / else. O usuário digita um número inteiro qualquer, após isso o sistema informa se o número digitado foi positivo ou negativo.

---
+++ testando_if_else.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class testando_if_else {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner (System.in);
+	
+	System.out.println("Digite um número inteiro, sendo ele negativo ou positivo: ");
+	int x = sc.nextInt();
+	
+	System.out.println();
+	
+	if (x < 0) {
+		System.out.println("O número digitado é um valor NEGATIVO.");
+		
+	}
+	
+	else {
+		System.out.println("O número digitado é um valor POSITIVO.");
+	
+	}
+	
+	sc.close();
+	
+	}
+
+}
