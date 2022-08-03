949c3c8c5b9becfb025b653d026ccff31acc7c0b
Aliane Elias Amaral <108065159+AlianeElias@users.noreply.github.com>
2022-08-03 07:39:29 -0400

    Add files via upload
    Código realizado para teste de IF - ELSE IF, para utilizar posteriormente para aprender a aplicação de da sintaxe opcional, switch case.

---
+++ testando_if_elseif.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class testando_if_elseif {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite o número para o dia da semana, de 1 até 7: ");
+		int numero = sc.nextInt();
+		String dia;
+		
+		if (numero == 1) {
+			dia = "DOMINGO.";
+		}
+		
+		else if (numero == 2) {
+			dia = "SEGUNDA-FEIRA.";
+		}
+		
+		else if (numero == 3) {
+			dia = "TERÇA-FEIRA.";
+		}
+		
+		else if (numero == 4) {
+			dia = "QUARTA-FEIRA.";
+		}
+		
+		else if (numero == 5) {
+			dia = "QUINTA-FEIRA.";
+		}
+		
+		else if (numero == 6) {
+			dia = "SEXTA-FEIRA.";
+		}
+		
+		else if (numero == 7) {
+			dia = "SÁBADO.";
+		}
+		
+		else {
+			dia = "NÚMERO INVÁLIDO.";
+		}
+		
+		System.out.println("Dia da Semana: " + dia);
+		
+		sc.close();
+
+	}
+
+}
