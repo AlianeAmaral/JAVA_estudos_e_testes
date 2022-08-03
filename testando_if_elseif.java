e626ed059a2d2a609c44e79e9888cd6eb7cbe54b
Aliane Elias Amaral <108065159+AlianeElias@users.noreply.github.com>
2022-08-03 07:53:23 -0400

    Update testando_if_elseif.java

--- testando_if_elseif.java
+++ testando_if_elseif.java
@@ -3,47 +3,38 @@ import java.util.Scanner;
 public class testando_if_elseif {
 
 	public static void main(String[] args) {
-	
+
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Digite o n˙mero para o dia da semana, de 1 atÈ 7: ");
+
+		System.out.println("Digite o n√∫mero para o dia da semana, de 1 at√© 7: ");
 		int numero = sc.nextInt();
 		String dia;
-		
+
 		if (numero == 1) {
 			dia = "DOMINGO.";
 		}
-		
 		else if (numero == 2) {
 			dia = "SEGUNDA-FEIRA.";
 		}
-		
 		else if (numero == 3) {
-			dia = "TER«A-FEIRA.";
+			dia = "TER√áA-FEIRA.";
 		}
-		
 		else if (numero == 4) {
 			dia = "QUARTA-FEIRA.";
 		}
-		
 		else if (numero == 5) {
 			dia = "QUINTA-FEIRA.";
 		}
-		
 		else if (numero == 6) {
 			dia = "SEXTA-FEIRA.";
 		}
-		
 		else if (numero == 7) {
-			dia = "S¡BADO.";
+			dia = "S√ÅBADO.";
 		}
-		
 		else {
-			dia = "N⁄MERO INV¡LIDO.";
+			dia = "N√öMERO INV√ÅLIDO.";
 		}
-		
 		System.out.println("Dia da Semana: " + dia);
-		
 		sc.close();
 
 	}
