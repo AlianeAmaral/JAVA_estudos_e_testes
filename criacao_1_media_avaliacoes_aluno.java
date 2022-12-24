ba0162fc55a516552dd01083117d9e7353b2b761
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2022-12-24 08:31:17 -0400

    criacao_1_media_avaliacoes_aluno.java
    O usuário pode digitar o nome do aluno e disciplina que está sendo avaliada, depois poderá inserir a nota que o aluno tirou separadamente em cada avaliação.
    O sistema irá apresentar o nome, disciplina e a média do aluno.

--- criacao_1_media_avaliacoes_aluno.java
+++ criacao_1_media_avaliacoes_aluno.java
@@ -14,23 +14,23 @@ public class criacao_1_media_avaliacoes_aluno {
 	String disciplina = sc.nextLine();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 1: ");
+	System.out.println("Insira a nota da AvaliaÃ§Ã£o 1: ");
 	double AV1 = sc.nextDouble();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 2: ");
+	System.out.println("Insira a nota da AvaliaÃ§Ã£o 2: ");
 	double AV2 = sc.nextDouble();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 3: ");
+	System.out.println("Insira a nota da AvaliaÃ§Ã£o 3: ");
 	double AV3 = sc.nextDouble();
 	
 	double media = (AV1 + AV2 + AV3) / 2;
 	
 	System.out.println();
-	System.out.println("A nota média do aluno(a) " + nome + " na disciplina " + disciplina + " é " + media);
+	System.out.println("A nota mÃ©dia do aluno(a) " + nome + " na disciplina " + disciplina + " Ã© " + media);
 	
-	sc.close();
+	sc.close(); 
 
 	}
 
