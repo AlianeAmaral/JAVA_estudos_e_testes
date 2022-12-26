bb69fce70034d4aadf53158e8be0a16579b9f634
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2022-12-26 07:36:09 -0400

    mdia_avaliacoes_aluno.java

--- media_avaliacoes_aluno.java
+++ media_avaliacoes_aluno.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class criacao_1_media_avaliacoes_aluno {
+public class media_avaliacoes_aluno {
 
 	public static void main(String[] args) {
 	
@@ -14,15 +14,15 @@ public class criacao_1_media_avaliacoes_aluno {
 	String disciplina = sc.nextLine();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 1: ");
+	System.out.println("Insira a nota da 1ª Nota: ");
 	double AV1 = sc.nextDouble();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 2: ");
+	System.out.println("Insira a nota da 2ª Nota: ");
 	double AV2 = sc.nextDouble();
 	
 	System.out.println();
-	System.out.println("Insira a nota da Avaliação 3: ");
+	System.out.println("Insira a nota da 3ª Nota: ");
 	double AV3 = sc.nextDouble();
 	
 	double media = (AV1 + AV2 + AV3) / 2;
@@ -31,7 +31,5 @@ public class criacao_1_media_avaliacoes_aluno {
 	System.out.println("A nota média do aluno(a) " + nome + " na disciplina " + disciplina + " é " + media);
 	
 	sc.close(); 
-
 	}
-
 }
