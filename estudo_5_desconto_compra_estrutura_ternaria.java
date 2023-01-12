f67b1575b50a3c7aa9a2dd848449a1b3ec5135b0
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-01-12 08:33:07 -0400

    Update estudo_5_desconto_compra_estrutura_ternaria.java

--- estudo_5_desconto_compra_estrutura_ternaria.java
+++ estudo_5_desconto_compra_estrutura_ternaria.java
@@ -6,13 +6,14 @@ public class estudo_5_desconto_compra_estrutura_ternaria {
 	
 	Scanner sc = new Scanner (System.in);
 	
-	System.out.println("Compre mais de R$ 100.00 e ganhe R$ 20,00 de desconto. Aproveite!");
+	System.out.println("Compre até ou mais de R$ 100.00 e ganhe R$ 20,00 de desconto. Aproveite!");
 	System.out.println("Digite o valor total da compra: R$ ");
 	
 	double preco = sc.nextDouble();
-	double desconto = (preco > 100.0)? preco*0.1:preco*0.2;
-	double precoFinal = preco - desconto;
+	double precoDesconto = (preco >= 100.0)? preco-20:preco-0;
 	
-	System.out.println("Total da sua compra é: " + precoFinal);
+	System.out.printf("Total da Compra: R$ %.2f %n", precoDesconto);
+	
+	sc.close();
 	}
 }
