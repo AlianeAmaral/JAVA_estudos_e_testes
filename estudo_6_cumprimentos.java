b0f0fd5287e5eb26363c8b27c0848d916e06e2e9
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-01-13 06:18:15 -0400

    estudo_6_cumprimentos.java

---
+++ estudo_6_cumprimentos.java
@@ -0,0 +1,28 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class estudo_6_cumprimentos {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	
+	System.out.println("Que horas são? (Obs: digitar o número inteiro das horas).");
+	int horas = sc.nextInt();
+	
+	if (horas < 12){
+		System.out.println("O cumprimento é: Bom dia!");
+	}
+	
+	else if (horas >= 12 && horas < 18){
+		System.out.println("O cumprimento é: Boa tarde!");
+	}
+	
+	else if (horas > 18){
+		System.out.println("O cumprimento é: Boa noite!");
+	}
+	
+	sc.close();
+	}
+
+}
