76222865e760ab84dd3c70e37ea869f640362505
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-01-13 06:19:35 -0400

    estudo_8_desconto_porcentagem_estrutura_ternaria.java

---
+++ estudo_8_desconto_porcentagem_estrutura_ternaria.java
@@ -0,0 +1,20 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class estudo_8_desconto_porcentagem_estrutura_ternaria {
+
+	public static void main(String[] args) {
+		
+	Scanner sc = new Scanner (System.in);
+		
+	System.out.println("Compre até ou mais de R$ 500.00 e ganhe 30% de desconto no valor total da compra. Aproveite!");
+	System.out.println("Digite o valor total da compra: R$ ");
+		
+	double preco = sc.nextDouble();
+	double precoFinal = (preco >= 500.0)? preco - (preco * 0.30) : preco - 0;
+		
+	System.out.printf("Total da Compra: R$ %.2f %n", precoFinal);
+		
+	sc.close();
+	}
+}
