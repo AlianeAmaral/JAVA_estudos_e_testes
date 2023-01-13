f4e90a9d1b246e0d5380bbf6ca343e6f6cc63989
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-01-13 06:19:16 -0400

    estudo_7_franquia_celular_atribuicao_cumulativa.java

---
+++ estudo_7_franquia_celular_atribuicao_cumulativa.java
@@ -0,0 +1,33 @@
+package estudos_programacao;
+import java.util.Scanner;
+
+public class estudo_7_franquia_celular_atribuicao_cumulativa {
+
+	public static void main(String[] args) {
+	
+	Scanner sc = new Scanner (System.in);
+
+	System.out.println("Quantos minutos da franquia foram utilizados?");
+	int minutosConsumo = sc.nextInt();
+	double excedente;
+	
+	if (minutosConsumo <= 100) {
+		System.out.println("Valor a Pagar: R$ 50,00");
+	}
+	
+	else if (minutosConsumo > 100) {
+		excedente = ((minutosConsumo - 100) * 2) + 50;
+		
+		// ou pode-se usar também: 
+		// double conta = 50;
+		
+		// conta = conta + (minutos - 100) * 2; ou também
+		// conta += (minutos - 100) * 2;
+		
+		System.out.printf("Valor a Pagar: R$ %.2f %n", excedente);
+	}
+	
+	sc.close();
+	}
+
+}
